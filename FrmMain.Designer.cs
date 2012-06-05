b23b7a773dec2b2a0627668ad6dae5e487126b6b
Evandro Magalhes Leite Jnior <evandrojr@gmail.com>
2012-06-05 15:20:26 -0300

    first commit

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace HorasTrabalhadas
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.lblHorasTrab = new System.Windows.Forms.Label();
+            this.linkLabel = new System.Windows.Forms.LinkLabel();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(6, 19);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 26);
+            this.txt1.TabIndex = 0;
+            this.txt1.Text = "9:00";
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
+            // 
+            // txt2
+            // 
+            this.txt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.Location = new System.Drawing.Point(6, 49);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 26);
+            this.txt2.TabIndex = 1;
+            this.txt2.Text = "12:00";
+            this.txt2.TextChanged += new System.EventHandler(this.txt2_TextChanged);
+            // 
+            // txt4
+            // 
+            this.txt4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt4.Location = new System.Drawing.Point(6, 109);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(100, 26);
+            this.txt4.TabIndex = 3;
+            this.txt4.Text = "18:00";
+            this.txt4.TextChanged += new System.EventHandler(this.txt4_TextChanged);
+            // 
+            // txt3
+            // 
+            this.txt3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt3.Location = new System.Drawing.Point(6, 79);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 26);
+            this.txt3.TabIndex = 2;
+            this.txt3.Text = "13:00";
+            this.txt3.TextChanged += new System.EventHandler(this.txt3_TextChanged);
+            // 
+            // lblHorasTrab
+            // 
+            this.lblHorasTrab.AutoSize = true;
+            this.lblHorasTrab.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHorasTrab.Location = new System.Drawing.Point(10, 164);
+            this.lblHorasTrab.Name = "lblHorasTrab";
+            this.lblHorasTrab.Size = new System.Drawing.Size(66, 25);
+            this.lblHorasTrab.TabIndex = 5;
+            this.lblHorasTrab.Text = "08:00";
+            // 
+            // linkLabel
+            // 
+            this.linkLabel.AutoSize = true;
+            this.linkLabel.Location = new System.Drawing.Point(6, 25);
+            this.linkLabel.Name = "linkLabel";
+            this.linkLabel.Size = new System.Drawing.Size(113, 13);
+            this.linkLabel.TabIndex = 6;
+            this.linkLabel.TabStop = true;
+            this.linkLabel.Text = "EvandroJr@gmail.com";
+            this.linkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_LinkClicked);
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.txt1);
+            this.groupBox.Controls.Add(this.txt2);
+            this.groupBox.Controls.Add(this.txt4);
+            this.groupBox.Controls.Add(this.txt3);
+            this.groupBox.Location = new System.Drawing.Point(8, 9);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(116, 146);
+            this.groupBox.TabIndex = 8;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Horários";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.linkLabel);
+            this.groupBox1.Location = new System.Drawing.Point(5, 194);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(122, 46);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sugestões para:";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(133, 248);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.lblHorasTrab);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FrmMain";
+            this.Text = "Horas trabalhadas";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Label lblHorasTrab;
+        private System.Windows.Forms.LinkLabel linkLabel;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HorasTrabalhadas
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        DateTime T1, T2, T3, T4;
+
+
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+            calc();
+        }
+
+        private void txt2_TextChanged(object sender, EventArgs e)
+        {
+            calc();
+        }
+
+        private void txt3_TextChanged(object sender, EventArgs e)
+        {
+            calc();
+        }
+
+        private void txt4_TextChanged(object sender, EventArgs e)
+        {
+            calc();
+        }
+
+
+        private void calc() {
+            try
+            {
+                T1 = Convert.ToDateTime(txt1.Text);
+                T2 = Convert.ToDateTime(txt2.Text);
+                T3 = Convert.ToDateTime(txt3.Text);
+                T4 = Convert.ToDateTime(txt4.Text);
+
+                TimeSpan manha = T2.Subtract(T1);
+                TimeSpan tarde = T4.Subtract(T3);
+                lblHorasTrab.Text=tarde.Add(manha).ToString();
+            }
+            catch { }
+            
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            calc();
+        }
+
+        private void linkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo.FileName = "mailto:evandrojr@gmail.com?subject=Programa Horas Trabalhadas";
+            proc.Start();
+
+        }
+
+
+
+    }
+}
